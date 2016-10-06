9947df3a17d48000fa11a946762d6e3d9920848c
Ranjith Sekar <java2ranjith@gmail.com>
2016-10-06 13:45:00 +0530

    updated

--- JavaCollectionsMap/src/main/java/jbr/treemap/TreeMapKeySorting.java
+++ JavaCollectionsMap/src/main/java/jbr/treemap/TreeMapKeySorting.java
@@ -4,7 +4,7 @@ import java.util.Comparator;
 import java.util.Map;
 import java.util.TreeMap;
 
-public class TreeMapSorting {
+public class TreeMapKeySorting {
   public static void main(String[] args) {
     System.out.println("Key=STRING");
     // default key sort is ascending, now sort key by descending using comparator
